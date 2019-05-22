785b43c5e7c008a4b083ccc675e3f247c3669084
정우찬 <vdncks25@naver.com>
2019-05-22 22:20:39 +0900

    FEAT : Apply props data to components
    prop 데이터를 component로 보내서 component에서 prop 데이터 값을 사용

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -2,13 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie'; 
 
+const movies = [
+  "IRON MAN", 
+  "SPIDER MAN : HomeComing", 
+  "THOR : Ragnarok", 
+  "AVENGERS : EndGame"
+]
+
+const movieImages = [
+  "http://mblogthumb3.phinf.naver.net/20141117_174/kimmisung08_1416159902342OA03t_JPEG/Iron_Man_poster%2Cmovie_poster%2C%BE%C6%C0%CC%BE%F0%B8%C7_%C6%F7%BD%BA%C5%CD%2C%BF%B5%C8%AD_%C6%F7%BD%BA%C5%CDdjfkl3jjdlkjflsdkf.jpg?type=w2",
+  "https://upload.wikimedia.org/wikipedia/ko/c/cb/%EC%8A%A4%ED%8C%8C%EC%9D%B4%EB%8D%94%EB%A7%A8_%ED%99%88%EC%BB%A4%EB%B0%8D.jpg",
+  "http://image.chosun.com/sitedata/image/201709/05/2017090500598_0.jpg",
+  "https://file.mk.co.kr/meet/neds/2019/04/image_readtop_2019_245869_15557132063718502.jpg"
+]
+
 // 모든 컴포넌트는 render를 가지고 있다. 
 // render의 기능은 뭔가를 보여주는, 출력하는 기능이다. 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie />
+        <Movie title={movies[0]} poster={movieImages[0]}/>
+        <Movie title={movies[1]} poster={movieImages[1]}/>
+        <Movie title={movies[2]} poster={movieImages[2]}/>
+        <Movie title={movies[3]} poster={movieImages[3]}/>
       </div>
     );
   }
--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -5,8 +5,8 @@ class Movie extends Component {
     render() {
         return (
             <div>
-                <MoviePoster/>
-                <h1>hello this is a movie</h1>
+                <MoviePoster poster={this.props.poster} title={this.props.title}/>
+                <h1>{this.props.title}</h1>
             </div>
         );
     }
@@ -15,7 +15,7 @@ class Movie extends Component {
 class MoviePoster extends Component {
     render() {
         return (
-            <img src="https://search.pstatic.net/common?type=o&size=120x171&quality=100&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20190417_250%2F1555465284425i6WQE_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2"/>
+            <img src={this.props.poster} alt={this.props.title}/>
         );
     }
 }
