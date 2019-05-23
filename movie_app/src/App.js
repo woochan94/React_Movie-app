17a47d0b4cf2df81f3381d3f05a046b717288310
정우찬 <vdncks25@naver.com>
2019-05-23 16:00:06 +0900

    FEAT : Add the ability to import movie api asynchronously
    fetch, async-await 를 이용하여 yts에서 영화 api를 비동기로 가져옴 
    비동기로 가져온 영화 데이터를 state에 넣어줌

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -9,15 +9,8 @@ class App extends Component {
   // React 생명주기 
   // Render : componentWillMount() -> render() -> componentDidMount()
   // Update : Update componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
-  
-  // State 
-  // Component 안에 state가 바뀔 때 마다 render가 발생한다.
-  
-  state = {
-    // greeting: 'Hello',
-    
-  }
 
+  state = {}
 
   componentWillMount = () => {
     console.log('will mount'); 
@@ -25,59 +18,28 @@ class App extends Component {
 
   componentDidMount = () => {
     console.log('did mount'); 
-    // setTimeout(() => {
-    //   this.setState({
-    //     greeting: 'Hello again!'
-    //   })
-    // }, 5000)
-
-    // setTimeout(() => {
-    //   this.setState({
-    //     movies: [
-    //       // 이전 state는 그대로 두고, 다음의 것을 추가하라는 것 
-    //       // ... 이 없으면 이전의 영화 리스트는 모두 없어지고 새로운 영화만 나타나게 됨 
-    //       ...this.state.movies, 
-    //       {
-    //         title: "GUARDIANS OF THE GALAXY2", 
-    //         poster: "https://post-phinf.pstatic.net/MjAxNzA1MDJfMTQ1/MDAxNDkzNjc3MDY1NTk5.7CzKn-qdgdKupsBGgano7zjPsz5K_VyK2edMxqXaOJsg.DqbcL2o5uSceBXF-abho1_2-_JbN5IXZc-TZEBAE-CEg.JPEG/movie_image.jpg?type=w1200"
-    //       }
-    //     ]
-    //   })
-    // },5000)
-
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "IRON MAN",
-            poster: "http://mblogthumb3.phinf.naver.net/20141117_174/kimmisung08_1416159902342OA03t_JPEG/Iron_Man_poster%2Cmovie_poster%2C%BE%C6%C0%CC%BE%F0%B8%C7_%C6%F7%BD%BA%C5%CD%2C%BF%B5%C8%AD_%C6%F7%BD%BA%C5%CDdjfkl3jjdlkjflsdkf.jpg?type=w2"
-          },
-          {
-            title: "SPIDER MAN : HomeComing",
-            poster: "https://upload.wikimedia.org/wikipedia/ko/c/cb/%EC%8A%A4%ED%8C%8C%EC%9D%B4%EB%8D%94%EB%A7%A8_%ED%99%88%EC%BB%A4%EB%B0%8D.jpg"
-          },
-          {
-            title: "THOR : Ragnarok",
-            poster: "http://image.chosun.com/sitedata/image/201709/05/2017090500598_0.jpg"
-          },
-          {
-            title: "AVENGERS : EndGame",
-            poster: "https://file.mk.co.kr/meet/neds/2019/04/image_readtop_2019_245869_15557132063718502.jpg"
-          }, 
-          {
-            title: "GUARDIANS OF THE GALAXY2", 
-            poster: "https://post-phinf.pstatic.net/MjAxNzA1MDJfMTQ1/MDAxNDkzNjc3MDY1NTk5.7CzKn-qdgdKupsBGgano7zjPsz5K_VyK2edMxqXaOJsg.DqbcL2o5uSceBXF-abho1_2-_JbN5IXZc-TZEBAE-CEg.JPEG/movie_image.jpg?type=w1200"
-          }
-        ]
-      })
-    }, 5000)
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map(movie => {
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
     })
     return movies;
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort by=rating')
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
   }
 
   render() {
